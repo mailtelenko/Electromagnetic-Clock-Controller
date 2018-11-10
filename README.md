deaf5fe618ceb2b0c87ba33ef306595a682715cc
Liam Telenko <mailtelenko@gmail.com>
2018-11-10 01:46:43 -0500

    First working version

--- README.md
+++ README.md
@@ -11,6 +11,20 @@ The circuit diagram can be found below the components list.
 
 1. Arduino (UNO)
 2. Magnetic coil
-3. Battery holder & batteries (~9 --> 12v)
-4. Transistor (?)
-5. Basic components & tools (wires/solder + iron/etc)
+3. L293D H-Bridge
+4. DC power adapter (~12v) & Barrel jack
+5. Potentiometers (x2) (optional)
+6. Buttons (x2) (optional)
+7. LED (optional)
+8. 220 ohm resistor (x2) (optional)
+9. 330 ohm resistor (optional)
+10. Basic components & tools (wires/solder + iron/etc)
+
+### Circuit
+![alt text](https://github.com/mailtelenko/Electromagnetic-Clock-Controller/clock-circuit.png "Clock circuit")
+
+## Setup
+The potentiometers and buttons can be used to adjust the timing and power of the electomagnet. Although not neccessary for operation, using the potentiometer and buttons makes adjusting the clock easier compared to editing the code directly.
+
+Once setup, the values can be set in the global variables section to ensure they stay constant during operation.
+
--- clock-circuit.desktop.ino
+++ clock-circuit.desktop.ino
@@ -11,26 +11,44 @@
 // Variables
 //
 // Pin references
-int ledPin = 2;
-int coilPin = A5;
+int ledPin = 8;
+int adjustmentPot_timing = A0;
+int adjustmentPot_power = A1;
+int fasterButton = 7;
+int slowerButton = 6;
+
+// Coil Pins
+int coilPWMPin = 5;
+int coilSwitch1 = 3;
+int coilSwitch2 = 4;
+
+// Coil controls
+int coilPower = 0;
 
 // Timing variables
-long currentTime = 0; // Global time 
-long periodStart = 0; // Start of the period 
+long currentTime = 0; // Global time
+long periodStart = 0; // Start of the period
 long nextPulse = 0; // The time at which the next pulse shoudld start --> Modified by calculateTiming
-long pulseLength = 0; // The duration of the pulse to be applied to the pendulum --> Modified by calculateTiming
+long pulseLength = 500; // The duration of the pulse to be applied to the pendulum --> Modified by calculateTiming
+long pulseSpeed = 1000; // The length of the period of pulses
 
 /*
- * setup method to initialize variables and pins.
- */
+   setup method to initialize variables and pins. Runs once
+   at startup.
+*/
 void setup() {
   Serial.begin(9600); //Open serial connection
 
   // Set pin modes
   pinMode(ledPin, OUTPUT);
-  pinMode(coilPin, INPUT);
-}
+  pinMode(coilPWMPin, OUTPUT);
+  pinMode(coilSwitch1, OUTPUT);
+  pinMode(coilSwitch2, OUTPUT);
 
+  // Turn on coil
+  digitalWrite(coilSwitch1, LOW);
+  digitalWrite(coilSwitch2, HIGH);
+}
 /*
    checkMagnet checks for a magnetic field from the copper coil
    and returns true if one is found.
@@ -39,6 +57,7 @@ void setup() {
 */
 bool checkMagnet() {
   //return analogRead(coilPin);
+  return true;
 }
 
 /*
@@ -53,6 +72,8 @@ void calculateTiming() {
       If the pendulum is not correctly timed (millis() - periodStart)
       then set the next pulse accordingly (period too long --> later, short --> sooner)
   */
+  if (periodStart + pulseLength < currentTime)
+    nextPulse = currentTime + (pulseSpeed - pulseLength);
 }
 
 /*
@@ -66,9 +87,35 @@ void calculateTiming() {
 void pulseCoil() {
   // Check if the current time is when the pulse should be fired
   // and if the time is within the length it sould be fired for.
-  if(millis() >= nextPulse && millis() <= nextPulse + pulseDuration)
-    // Turn magnetic coil on
-  // Turn off coil
+  if (millis() >= nextPulse && millis() <= nextPulse + pulseLength) {
+    digitalWrite(ledPin, HIGH); // Turn on LED
+    analogWrite(coilPWMPin, coilPower); // Turn on power to coil
+  } else {
+    digitalWrite(ledPin, LOW);// Turn off LED
+    analogWrite(coilPWMPin, 0); // Shut off power to coil
+  }
+
+}
+
+/*
+   modifyTiming uses potentiometer input to change the duration of the
+   pendulum swing.
+*/
+void modifyTiming() {
+  pulseLength = analogRead(adjustmentPot_timing);
+  coilPower = analogRead(adjustmentPot_power);
+
+  if(digitalRead(fasterButton) == HIGH)
+    pulseSpeed += 1;
+  else if(digitalRead(slowerButton) == HIGH)
+    pulseSpeed -= 1;
+
+  Serial.print("Pulse Length: ");
+  Serial.print(pulseLength);
+  Serial.print(" | Power: ");
+  Serial.print(coilPower);
+  Serial.print(" | Period: ");
+  Serial.println(pulseSpeed);
 }
 
 /*
@@ -78,8 +125,10 @@ void loop() {
   currentTime = millis(); // Set current time to the amount of time since board turned on
 
   // if checkMagnet() is true --> calculate when to pulse magnet + strength based on time of swing
-  if (checkMagnet() && nextPulse < millis())
+  if (checkMagnet() && nextPulse + pulseLength < millis())
     calculateTiming(); // Calculate next time to pulse coil
 
+  if (currentTime % 200 == 0)
+    modifyTiming(); // Check potentiometer to adjust timing of pendulum
   pulseCoil(); // Run pulseCoil to check if coil should be on
 }
